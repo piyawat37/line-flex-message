b7befddd681852bd29ba6f3c114a22bb4d966fd1
Piyawat Pemwattana <piyawat.pemwattana@gmail.com>
2020-01-09 14:19:42 +0700

    fixed bug container

--- src/contents/Container.ts
+++ src/contents/Container.ts
@@ -5,14 +5,14 @@ export class Container {
     public type: containerType
     public size: containerSize
     public direction: containerDirection
-    public body: Body
-    public footer: Footer
+    public body?: Body
+    public footer?: Footer
 
     constructor(type?: containerType, size?: containerSize, direction?: containerDirection, body?: Body, footer?: Footer) {
         this.type = type || "bubble"
         this.size = size || "mega"
         this.direction = direction || "ltr"
-        this.body = body || new Body()
-        this.footer = footer || new Footer()
+        this.body = body || undefined
+        this.footer = footer || undefined
     }
 }
