1432f7828e24fcc5a7a78cab4b2d30e424a95d06
Piyawat Pemwattana <piyawat.pemwattana@gmail.com>
2020-01-09 15:34:11 +0700

    dispatch spacing from button

--- src/elements/Button.ts
+++ src/elements/Button.ts
@@ -3,19 +3,16 @@ import {buttonStyle, sizeOptions} from "../types";
 
 export class Button {
     public type: string
-    public spacing: sizeOptions
     public action: ButtonAction;
     public style: buttonStyle
     public color?: string
 
     constructor(
-        spacing?: sizeOptions,
         action?: ButtonAction,
         style?: buttonStyle,
         color?: string
     ) {
         this.type = "button"
-        this.spacing = spacing || "md"
         this.action = action || new ButtonAction()
         this.style = style || "link"
         this.color = color || undefined
